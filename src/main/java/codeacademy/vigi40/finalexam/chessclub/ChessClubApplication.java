c305f90e45bebdb1ff90b815f7d500975b2c26d6
GretaKi <greta.kisunaite@gmail.com>
2023-03-12 15:34:42 +0200

    Init spring boot

---
+++ src/main/java/codeacademy/vigi40/finalexam/chessclub/ChessClubApplication.java
@@ -0,0 +1,13 @@
+package codeacademy.vigi40.finalexam.chessclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeacademy/vigi40/finalexam/chessclub/ChessClubApplicationTests.java
@@ -0,0 +1,13 @@
+package codeacademy.vigi40.finalexam.chessclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
